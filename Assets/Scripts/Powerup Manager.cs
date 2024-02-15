3432317f44fab4183dbafc0f3a6551e2b034e900
JudahSilva <jusilva@csumb.edu>
2024-02-15 14:32:14 -0800

    Added powerup manager

---
+++ Assets/Scripts/Powerup Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    public GameObject powerupCube;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time t = new Time();
+        Debug.Log(t);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
